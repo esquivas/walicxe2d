fa6f401cee87f289e823d6645386faa744de39f8
~/.gitconfig <esquivel@nemesis.nucleares.unam.mx>
2014-09-29 15:36:14 -0500

    removed extra !s
    needed in order to later count correctly  the number of code and
    comment lines

--- src/sn.f90
+++ src/sn.f90
@@ -28,33 +28,33 @@ subroutine impose_sn(u, nb, xc, yc)
   real    :: ekin, x, y, r, dens, vr, Eth, Mtot
 
   lev=lp(nb,1)
-  !
+  
   Mtot=Msn+(4.*pi/3.)*nenv*rhosc*Rsn**3
   !print*,Msn, Mtot, (4.*pi/3.)*nenv*rhosc*Rsn**3
-  !
+  
   do i=nxmin,nxmax
      do j=nymin,nymax
-        !
+        
         x= ( float(i+icoord(nb,1)) -0.5 )* dx(lev)*rsc
         y= ( float(j+icoord(nb,2)) -0.5 )* dy(lev)*rsc
-        !
+        
         R=sqrt((x-xc)**2+(y-yc)**2+0.1*dx(nlevs)*rsc )
-        !
+        
         if (r.le.Rsn) then
-           !
+           
            !   inside SN
            dens=(3./4./pi)*Msn/(Rsn**3)*1.3/2.1 /rhosc!  + nenv
            vr=(R/Rsn)*Sqrt(10*chi*Esn/(3*Msn))  /sqrt(vsc2)
            Eth=(3./4./pi)*Esn*(1.-chi)/(Rsn**3) /psc
-           !
+           
            u(1,i,j)= dens
            u(2,i,j)= dens*vr*(x-xc)/(R)
            u(3,i,j)= dens*vr*(y-yc)/(R)
            u(4,i,j)= 0.5*dens*vr**2+Eth
-           !
+           
            u(5,i,j)=  dens*0.0  !  neutral fraction
            u(6,i,j)=  dens
-           !
+           
         endif
      end do
   end do
